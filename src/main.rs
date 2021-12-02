76ba117dd5f8e0854321cdb7b2052e0cc3ab520f
relam <relam@rcschools.us>
2021-12-02 11:24:42 -0500

    io test

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,21 @@ pub mod types;
 use std::collections::HashMap;
 use std::fmt::Debug;
 use std::time::{Duration, Instant};
-
-use websocket::message::OwnedMessage;
-use websocket::server::InvalidConnection;
-use websocket::server::r#async::Server;
-
 use std::sync::{Arc, RwLock};
+use std::io;
+use std::io::prelude::*;
 
 fn main() {
-    println!("Hello, world!");
+    println!("Test input");
+
+    let stdin = io::stdin();
+
+    for line in stdin.lock().lines() {
+        let line = line.unwrap();
+        println!("Output: {}", line);
+
+        if line.to_lowercase() == "exit" {
+            break;
+        }
+    }
 }
