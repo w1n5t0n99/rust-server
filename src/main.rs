0f4f09076a6631ce887c8e837ce1015f8b7dcadd
relam <relam@rcschools.us>
2021-12-08 13:43:09 -0500

    add dependencies

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,32 @@ use std::sync::{Arc, RwLock};
 use std::io;
 use std::io::prelude::*;
 
-fn main() {
+use futures::{Future, StreamExt, TryStreamExt};
+use tokio::net::{TcpListener, TcpStream};
+
+use anyhow::Result;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    
+    /*
     println!("Test input");
 
     let stdin = io::stdin();
-
     for line in stdin.lock().lines() {
         let line = line.unwrap();
         println!("Output: {}", line);
+
+
 
         if line.to_lowercase() == "exit" {
             break;
         }
     }
+    */
+
+     // Create the event loop and TCP listener we'll accept connections on.
+     let try_socket = TcpListener::bind("127.0.0.1:8080").await;
+
+    Ok(())
 }
