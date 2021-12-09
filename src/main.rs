9e4bb7babb42ff1a4f4c02c3ab7b0b4edd920808
relam <relam@rcschools.us>
2021-12-09 13:46:56 -0500

    add echo server

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,26 @@ use std::sync::{Arc, RwLock};
 use std::io;
 use std::io::prelude::*;
 
-use futures::{Future, StreamExt, TryStreamExt};
+use futures::{future, StreamExt, TryStreamExt};
 use tokio::net::{TcpListener, TcpStream};
 
 use anyhow::Result;
+
+async fn accept_connection(stream: TcpStream) {
+    let addr = stream.peer_addr().expect("connected streams should have a peer address");
+    println!("{}", addr);
+
+    let ws_stream = tokio_tungstenite::accept_async(stream)
+        .await
+        .expect("error during websocket handshake occurred");
+
+    let (write, read) = ws_stream.split();
+    // we should not forward messages other than text or binary
+    read.try_filter(|msg| future::ready(msg.is_text() || msg.is_binary()))
+        .forward(write)
+        .await
+        .expect("failed to forward message");
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -32,7 +48,12 @@ async fn main() -> Result<()> {
     */
 
      // Create the event loop and TCP listener we'll accept connections on.
-     let try_socket = TcpListener::bind("127.0.0.1:8080").await;
+     let listener = TcpListener::bind("127.0.0.1:8080").await.expect("can't listen");
+
+     while let Ok((stream, addr)) = listener.accept().await {
+         println!("socket address: {}", addr);
+        tokio::spawn(accept_connection(stream));
+     }
 
     Ok(())
 }
