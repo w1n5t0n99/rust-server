953bdfc496c7f0de44d361108b4cdf53aa9f7fd5
relam <relam@rcschools.us>
2021-12-14 14:05:40 -0500

    refactor client

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,7 +1,12 @@
+use futures::channel::mpsc::UnboundedReceiver;
 use futures::{future, StreamExt, TryStreamExt, pin_mut};
+use futures::stream::{SplitSink, SplitStream};
 
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use tokio_tungstenite::{connect_async, tungstenite::protocol::Message};
+use tokio::sync::{broadcast, mpsc};
+use tokio::net::TcpStream;
+
+use tokio_tungstenite::{connect_async, tungstenite::protocol::Message, WebSocketStream, MaybeTlsStream};
 
 use anyhow::Result;
 
@@ -18,16 +23,42 @@ async fn read_stdin(tx: futures::channel::mpsc::UnboundedSender<Message>) {
     }
 }
 
-async fn exit_signal(tx: futures::channel::mpsc::UnboundedSender<Message>) {
+async fn stdin_to_ws(write: SplitSink<WebSocketStream<MaybeTlsStream<TcpStream>>, Message>, stdin_rx: UnboundedReceiver<Message>) {
+    stdin_rx.map(|m| Ok(m)).forward(write);
+}
+
+async fn ws_to_stdout(read: SplitStream<WebSocketStream<MaybeTlsStream<TcpStream>>>) {
+    read.for_each(|message| async {
+        match message {
+            Ok(message) => {
+                let output_msg = format!("server: {}", message.into_text().unwrap());
+                tokio::io::stdout().write_all(output_msg.as_bytes()).await.unwrap();
+            }
+            Err(_) => {
+                println!("error reading message");
+            }
+        }
+    });
+}
+
+async fn exit_signal(tx: broadcast::Sender<u8>) {
     tokio::signal::ctrl_c().await.expect("signal error");
-    tx.unbounded_send(Message::Close(None)).unwrap();
+    tx.send(0).unwrap();
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
    
     let (stdin_tx, stdin_rx) = futures::channel::mpsc::unbounded();
-    tokio::spawn(read_stdin(stdin_tx.clone()));
+    let (tx, mut rx1) = broadcast::channel::<u8>(16);
+    //let mut rx2 = tx.subscribe();
+
+   // tokio::spawn(read_stdin(stdin_tx.clone()));
+
+    tokio::select! {
+        _ = read_stdin(stdin_tx.clone()) => { }
+        _ = rx1.recv() => { }
+    };
 
     let url = url::Url::parse("ws:////127.0.0.1:8080").unwrap();
 
@@ -38,25 +69,16 @@ async fn main() -> Result<()> {
 
     let (write, read) = ws_stream.split();
 
-    let stdin_to_ws = stdin_rx.map(|m| Ok(m)).forward(write);
-
-    let ws_to_stdout = {
-        read.for_each(|message| async {
-            let data = message.unwrap().into_text().unwrap();
-            let output_msg = format!("server: {}", data);
-            tokio::io::stdout().write_all(output_msg.as_bytes()).await.unwrap();
-        })
-    };
 
     //pin_mut!(stdin_to_ws, ws_to_stdout);
     //future::select(stdin_to_ws, ws_to_stdout).await;
     
     tokio::select! {
-        _ = stdin_to_ws => {  println!("client input exiting"); }
+        _ = stdin_to_ws(write, stdin_rx) => {  println!("client input exiting"); }
 
-        _ = ws_to_stdout => {  println!("client output exiting"); }
+        _ = ws_to_stdout(read) => {  println!("client output exiting"); }
 
-        _ = exit_signal(stdin_tx.clone()) => {
+        _ = exit_signal(tx) => {
             println!("Got it... exiting");
         }
     }
