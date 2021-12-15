f11763be46d99bb0c5a24d5855339c37cb2d5d2d
relam <relam@rcschools.us>
2021-12-15 13:59:49 -0500

    async shut down cleanup

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -4,7 +4,8 @@ use futures::{future, StreamExt, TryStreamExt, pin_mut};
 use futures::stream::{SplitSink, SplitStream};
 
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use tokio::sync::{broadcast, mpsc};
+use tokio::sync::{broadcast};
+use tokio::sync::mpsc::{channel, Sender};
 use tokio::net::TcpStream;
 
 use tokio_tungstenite::{connect_async, tungstenite::protocol::Message, WebSocketStream, MaybeTlsStream};
@@ -27,7 +28,14 @@ async fn read_stdin(tx: futures::channel::mpsc::UnboundedSender<Message>) {
         let res = tx.unbounded_send(Message::binary(buf));
         if res.is_err() { break; }
     }
+}
 
+async fn rs(tx: futures::channel::mpsc::UnboundedSender<Message>, mut shutdown: shutdown::Shutdown, _sender: Sender<()>) {
+    tokio::select! {
+        _ = read_stdin(tx) => { }
+
+        _ = shutdown.recv() => { println!("read_stdin shutting down");  }
+    }
 }
 
 async fn stdin_to_ws(write: SplitSink<WebSocketStream<MaybeTlsStream<TcpStream>>, Message>, stdin_rx: UnboundedReceiver<Message>) {
@@ -39,6 +47,16 @@ async fn stdin_to_ws(write: SplitSink<WebSocketStream<MaybeTlsStream<TcpStream>>
     match res {
         Ok(_) => { }
         Err(_) => { println!("error sending input to server"); }
+    }
+
+    println!("stdin_to_ws ended");
+}
+
+async fn stw(write: SplitSink<WebSocketStream<MaybeTlsStream<TcpStream>>, Message>, stdin_rx: UnboundedReceiver<Message>, mut shutdown: shutdown::Shutdown, _sender: Sender<()>) {
+    tokio::select! {
+        _ = stdin_to_ws(write, stdin_rx) => { }
+
+        _ = shutdown.recv() => { println!("stdin_to_ws shutting down");  }
     }
 }
 
@@ -54,30 +72,28 @@ async fn ws_to_stdout(read: SplitStream<WebSocketStream<MaybeTlsStream<TcpStream
             }
         }
     }).await;
+
+    println!("ws_to_stdout ended");
 }
 
-async fn wts(read: SplitStream<WebSocketStream<MaybeTlsStream<TcpStream>>>, mut shutdown: shutdown::Shutdown) {
+async fn wts(read: SplitStream<WebSocketStream<MaybeTlsStream<TcpStream>>>, mut shutdown: shutdown::Shutdown, _sender: Sender<()>) {
     tokio::select! {
         _ = ws_to_stdout(read) => { }
-
+        
         _ = shutdown.recv() => { println!("ws_to_stdout shutting down");  }
     }
 }
 
-async fn exit_signal(tx: broadcast::Sender<()>) {
+async fn exit_signal(tx: broadcast::Sender<()>, _sender: Sender<()>) {
     tokio::signal::ctrl_c().await.expect("signal error");
     tx.send(()).unwrap();
+    println!("exit signal ended");
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
    
     let (stdin_tx, stdin_rx) = futures::channel::mpsc::unbounded();
-
-    let (tx, _rx1) = broadcast::channel(16);
-    let mut shutdown0 = shutdown::Shutdown::new(tx.subscribe()); 
-    let mut shutdown1 = shutdown::Shutdown::new(tx.subscribe()); 
-    let mut shutdown2 = shutdown::Shutdown::new(tx.subscribe()); 
 
     let url = url::Url::parse("ws:////127.0.0.1:8080").unwrap();
     println!("server url: {}", url);
@@ -86,23 +102,20 @@ async fn main() -> Result<()> {
     println!("websocket handshake successfully completed");
 
     let (write, read) = ws_stream.split();
+    let (tx, _) = broadcast::channel(16);
+    let (send_gaurd, mut rec_gaurd) = channel(1);
 
-    tokio::spawn(exit_signal(tx));
-    let stw_handle = tokio::spawn(stdin_to_ws(write, stdin_rx));
-    tokio::spawn(wts(read, shutdown1));
-    let rts_handle = tokio::spawn(read_stdin(stdin_tx.clone()));
+    tokio::spawn(stw(write, stdin_rx, shutdown::Shutdown::new(tx.subscribe()), send_gaurd.clone()));
+    tokio::spawn(wts(read, shutdown::Shutdown::new(tx.subscribe()), send_gaurd.clone()));
+    tokio::spawn(rs(stdin_tx.clone(), shutdown::Shutdown::new(tx.subscribe()), send_gaurd.clone()));
+    tokio::spawn(exit_signal(tx, send_gaurd.clone()));
 
-    tokio::select! {
-        _ = stw_handle => {  println!("client input exiting"); }
 
-        _ = shutdown0.recv() => { println!("stdin_to_ws shutting down");  }
-    }
     
-    tokio::select! {
-        _ = rts_handle => { }
+    drop(send_gaurd);
 
-        _ = shutdown2.recv() => { println!("read_stdin shutting down");  }
-    }
-    
+    let _ = rec_gaurd.recv().await;
+    println!("programm ended");
+
     Ok(())
 }
