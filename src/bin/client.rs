300bc56384854f25e9a648c01182ad00008ce57a
relam <relam@rcschools.us>
2022-04-12 13:59:22 -0400

    refactor test code

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,12 +1,15 @@
-use std::time::{Duration, Instant};
+use std::{time::{Duration, Instant}};
+use futures::stream::SplitStream;
 //use std::sync::mpsc::{channel, Sender, Receiver};
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpStream;
+
 //use tokio::sync::{broadcast};
 //use tokio::sync::mpsc;
-//use tokio::net::TcpStream;
 use futures_util::{future, pin_mut, StreamExt};
 use tokio_tungstenite::{connect_async, tungstenite::protocol::Message, WebSocketStream, MaybeTlsStream};
 use anyhow::Result;
+use tracing::{info_span, Instrument};
 use std::env;
 
 type MsgUnboundedReciever = futures::channel::mpsc::UnboundedReceiver<Message>;
@@ -20,6 +23,7 @@ async fn main() {
     let url = url::Url::parse(&connect_addr).unwrap();
 
     let (stdin_tx, stdin_rx) = futures::channel::mpsc::unbounded();
+
     tokio::spawn(read_stdin(stdin_tx));
 
     let (ws_stream, _) = connect_async(url).await.expect("Failed to connect");
@@ -39,16 +43,37 @@ async fn main() {
     future::select(stdin_to_ws, ws_to_stdout).await;
 }
 
-// Our helper method which will read data from stdin and send it along the
-// sender provided.
+
+async fn handle_incoming_messages(read: SplitStream<WebSocketStream<MaybeTlsStream<TcpStream>>>) {
+    read.for_each(|message| async {
+        let data = message.unwrap().into_data();
+        tokio::io::stdout().write_all(&data).await.unwrap();
+    });
+}
+
+
+#[tracing::instrument(
+    name = "Read stdin task",
+    skip(tx),
+)]
 async fn read_stdin(tx: MsgUnboundedSender) {
+
     let mut stdin = tokio::io::stdin();
     loop {
+        let span = info_span!("stdin read");
+
         let mut buf = vec![0; 1024];
-        let n = match stdin.read(&mut buf).await {
-            Err(_) | Ok(0) => break,
+        let n = match stdin.read(&mut buf)
+            .instrument(span)
+            .await {
+            Err(e) => { 
+                tracing::error!("Failed to read stdin: {:?}",e);
+                break;
+            },
+            Ok(0) => break,
             Ok(n) => n,
         };
+
         buf.truncate(n);
         tx.unbounded_send(Message::binary(buf)).unwrap();
     }
